135825a4d504f40f1a5b364f8f3372d47b37beab
muttboy <randallpq@gmail.com>
2023-07-10 23:31:49 -0700

    Added function to add color to grid cell

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
-// Creates a 16x16 grid
-function makeGrid() {
-    for (i = 0; i < 16; i++) {
-        let row = document.createElement('div');
-        row.className = 'row';
-        for (j = 0; j < 16; j++) {
-            let column = document.createElement('div');
-            column.className = 'column';
-            row.appendChild(column);
+let choice = 16
+let colorValue = "#808080"
+const colorButton = document.querySelectorAll('button');
+const sizeButton = document.querySelectorAll('input');
+
+
+
+// Creates a grid
+function makeGrid(choice) {
+    for (i = 0; i < choice; i++) {
+        let column = document.createElement('div');
+        column.className = 'column';
+        for (j = 0; j < choice; j++) {
+            let box = document.createElement('div');
+            box.className = 'box';
+            column.appendChild(box);
         }
-        document.getElementById('grid').appendChild(row);
+        document.getElementById('grid').appendChild(column);
     }
+
+    paintBox(colorValue);
 }
 
-makeGrid();
+// Changes color of each box in the grid when clicked
+function paintBox(colorValue) {
+    const boxes = document.querySelectorAll('.box');
+
+    boxes.forEach(box => {
+        box.addEventListener('click', () => {
+            box.style.backgroundColor = colorValue;
+        })
+    })
+}
+
+// Chooses color to be used for grid
+colorButton.forEach(color => {
+    color.addEventListener('click', function() {
+        paintBox(color.id);
+    })
+})
+
+makeGrid(choice);
+
+
